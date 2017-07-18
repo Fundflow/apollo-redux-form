83cef4c0962e0717f3da6beb4e4d3b8223783a00
mstn <mstn@users.noreply.github.com>
2017-07-18 19:40:58 +0200

    Create new release

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var apolloForm_1 = require("./apolloForm");
+exports.apolloForm = apolloForm_1.apolloForm;
+exports.buildForm = apolloForm_1.buildForm;
+exports.initForm = apolloForm_1.initForm;
+//# sourceMappingURL=index.js.map
