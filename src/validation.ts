28b5c25ecd5c54410a2f7addb18949651fcdc405
mstn <mstn@users.noreply.github.com>
2017-07-29 08:16:00 +0200

    make the empty object a non null value. This closes #20

--- src/validation.ts
+++ src/validation.ts
@@ -12,7 +12,7 @@ function validateImpl(
     const isRequired = field.props.required;
     const value = values[fieldName];
 
-    if ( isRequired && ( _.isEmpty(value) || _.isNil(value) )) {
+    if ( isRequired && (  (_.isEmpty(value) && !_.isObject(value)) || _.isNil(value) )) {
       errors[ fieldName ] = 'Required field.';
     } else {
       if ( children && children.length > 0 ) {
