5ad05cd39c5bfe3e2e4da347c995fe2114bda3e7
mstn <mstn@users.noreply.github.com>
2017-10-06 20:01:06 +0200

    use antd in storybook

--- stories/index.js
+++ stories/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { storiesOf, action, linkTo } from '@kadira/storybook'
 
+import 'antd/dist/antd.css';
+import { LocaleProvider } from 'antd';
+const enUS = require('antd/lib/locale-provider/en_US');
+
 import ApolloClient, { NetworkInterface } from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 
-import { createStore, combineReducers, applyMiddleware, Reducer } from 'redux'
+import { createStore, combineReducers, applyMiddleware, Reducer, compose } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import { Provider } from 'react-redux'
 
@@ -30,14 +34,19 @@ const store = createStore(
     apollo: client.reducer()
   }),
   {}, // init state
-  applyMiddleware(client.middleware())
+  compose(
+      applyMiddleware(client.middleware()),
+      (typeof window['__REDUX_DEVTOOLS_EXTENSION__'] !== 'undefined') ? window['__REDUX_DEVTOOLS_EXTENSION__']() : (f: any) => f,
+  )
 );
 
 storiesOf('Create post', module)
   .add('send data via Apollo', () => (
     <ApolloProvider client={client}>
       <Provider store={store}>
-        <CreatePostForm />
+          <LocaleProvider locale={enUS}>
+            <CreatePostForm />
+          </LocaleProvider>
       </Provider>
     </ApolloProvider>
   ));
@@ -46,7 +55,9 @@ storiesOf('Create user', module)
     .add('with separate section for company info', () => (
         <ApolloProvider client={client}>
             <Provider store={store}>
-                <CreateUserForm />
+                <LocaleProvider locale={enUS}>
+                    <CreateUserForm />
+                </LocaleProvider>
             </Provider>
         </ApolloProvider>
     ));
