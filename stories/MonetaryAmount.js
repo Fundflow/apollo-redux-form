2160ed214a2287dd01f2aee52063b23d0a511b00
mstn <mstn@users.noreply.github.com>
2017-11-09 17:19:05 +0100

    improve monetary amount example

--- stories/MonetaryAmount.js
+++ stories/MonetaryAmount.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import defaultRenderers from './defaultRenderers';
 
-import { Input, Select, InputNumber, Form} from 'antd';
+import { Input, Select, Form} from 'antd';
 const InputGroup = Input.Group;
 const Option = Select.Option;
 
@@ -12,14 +12,12 @@ import {
 } from '../lib/src/index';
 
 import {
-  capitalizeFirstLetter
+  isValidCurrencyValue
 } from './utils';
 
 const schema = gql`
-  scalar MoneyValue 
-  
   input MoneyInput {
-    value: MoneyValue
+    value: Float
     currency: Currency
   }
   enum Currency {
@@ -40,32 +38,68 @@ const MonetaryAmount = apolloForm(query, {
   schema,
   renderers: {
     ...defaultRenderers,
-    MoneyInput: (props) => {
-      return (
-        <Form.Item label={"Monetary Amount"}>
-          <InputGroup compact>
-            {props.children}
-          </InputGroup>
-        </Form.Item>
-      );
-    },
-    Currency: (props) => {
-      const { input: {onChange, value}, label, meta: {  error }} = props;
-      return (
-        <Select onSelect={onChange} value={value || 'EUR'}>
-          {
-            props.options.map( (option) => {
-              return <Option key={option.key} value={option.key}>{option.value}</Option>
-            })
+    MoneyInput: {
+      render(props) {
+        return (
+            <InputGroup compact>
+              {props.children}
+            </InputGroup>
+        );
+      },
+      renderers: {
+        Currency: {
+          render: (props) => {
+            const {input: {onChange, value}, label, meta: {error}} = props;
+            return (
+              <Form.Item
+                validateStatus={error ? 'error' : undefined}
+                help={error}
+              >
+                <Select onSelect={onChange} value={value} placeholder="Select a currency">
+                  {
+                    props.options.map((option) => {
+                      return <Option key={option.key} value={option.key}>{option.value}</Option>
+                    })
+                  }
+                </Select>
+              </Form.Item>
+            );
+          },
+          validate: (value, allValues) => {
+            if (!value && allValues.money && allValues.money.value !== undefined ) return 'Missing currency';
+            return null;
           }
-        </Select>
-      );
-    },
-    MoneyValue: (props) => {
-      const {input} = props;
-      return (
-        <InputNumber {...input} />
-      );
+        },
+        Float: {
+          render: (props) => {
+            const { input, meta: { error }} = props;
+            return (
+              <Form.Item
+                validateStatus={error ? 'error' : undefined}
+                help={error}
+              >
+                <Input {...input} />
+              </Form.Item>
+            );
+          },
+          normalize: (value) => {
+            return value.replace(',', '.');
+          },
+          format: (value) => {
+            return value && value.replace('.', ',');
+          },
+          validate: (value, allValues) => {
+            if (value == undefined || value == null ) {
+              if (allValues.money && allValues.money.currency) {
+                return 'Missing value.';
+              }
+            } else if (!isValidCurrencyValue(value)) {
+              return 'Invalid currency format.';
+            }
+            return null;
+          }
+        }
+      }
     }
   }
 });
